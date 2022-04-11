5cae08f541fe837c8217a42b7af8529e67a48cbf
Joe Maples <joe@maples.dev>
2022-04-11 14:35:49 -0400

    fix: Generate version.h
    Change-Id: I2839b6ad3b2cd24ce1d8d5989e97e6567ef572ce

---
+++ version.h
@@ -0,0 +1,8 @@
+#ifndef _rk_graphics_rga_version_h_
+#define _rk_graphics_rga_version_h_
+
+#define RGA_IM2D_VERSION "1.11"
+#define RGA_LIB_VERSION "1.04"
+#define RGA_BUILT_VERSION "9a7b3ca+2022-04-11 14:34:58"
+
+#endif /* _rk_graphics_rga_version_h_ */
